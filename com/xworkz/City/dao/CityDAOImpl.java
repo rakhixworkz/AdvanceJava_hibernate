d0e74c75129e8f52b7c126a109003e0883e69cb6
rakhixworkz <89894598+rakhixworkz@users.noreply.github.com>
2022-01-11 10:00:46 +0530

    Update CityDAOImpl.java

--- com/xworkz/City/dao/CityDAOImpl.java
+++ com/xworkz/City/dao/CityDAOImpl.java
@@ -27,6 +27,75 @@ public class CityDAOImpl implements CityDAO{
 		}
 		factrory.close();
 	}
+	@Override
+	public CityEntity getById(int id) {
+		CityEntity cityentity = null;
+		System.out.println("invked getbyid");
+		System.out.println("id passed as argument " + id);
+
+		SessionFactory sessionfactory = new Configuration().configure().addAnnotatedClass(CompanyEntity.class)
+				.buildSessionFactory();
+		if (sessionfactory != null) {
+			Session session = sessionfactory.openSession();
+			cityentity = session.get(CityEntity.class, id);
+			if (cityentity != null) {
+				System.out.println("passing id is found");
+			} else {
+				System.out.println("id not found");
+			}
+		}
+		return cityentity;
+	}
+
+	@Override
+	public void updateById(String name, int id) {
+		System.out.println("invode by id and update");
+
+		System.out.println("passing arg " + name + " " + id);
+
+		SessionFactory factory = new Configuration().configure().addAnnotatedClass(CityEntity.class)
+				.buildSessionFactory();
+		if (factory != null) {
+
+			Session session = factory.openSession();
+			Transaction tran = session.beginTransaction();
+			CityEntity entity = session.get(CityEntity.class, id);
+			if (entity != null) {
+
+				entity.setName(name);
+				session.update(entity);
+				tran.commit();
+				System.out.println("entity updated");
+				session.close();
+
+			} else {
+				System.out.println("not found wrong passing");
+			}
+			factory.close();
+
+		}
+	}
+	@Override
+	public void deleteById(int id) {
+		System.out.println("invoked delete by id");
+
+		SessionFactory factory= new Configuration().configure().addAnnotatedClass(CityEntity.class).buildSessionFactory(); 		
+		if(factory!=null) {
+			Session session =factory.openSession();
+			Transaction transaction=session.beginTransaction();
+			CityEntity entity=session.get(CityEntity.class, id);
+			if(entity!=null) {
+				session.delete(entity);
+				transaction.commit();
+				System.out.println("you are genius found that and deleted");
+				session.close();
+			}else {
+				System.out.println("not found that id");
+			}
+		}factory.close();
+
+	}
+	
 
 	
 }
